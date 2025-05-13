e6864af43c7e8652940f85990143f14e9272f7da
Kai Pepler <hello@kaipepler.com>
2025-05-13 19:45:24 -0400

    Typos & small fixes

--- src/routes/accessibility/+page.svelte
+++ src/routes/accessibility/+page.svelte
@@ -1,52 +1,52 @@
 <script>
 	const features = [
 		{
-			title: 'Increase Contrast',
-			desc: 'If you’re on macOS, the site will respect OS-level Increase Contrast mode, heighting the distinction between colors and outlining click targets across all pages.',
-			status: 'In development, not yet available',
-			icon: 'increase-contrast'
-		},
-		{
-			title: 'Reduce Motion',
-			desc: 'The site respects OS-level Reduce Motion requests, eliminating all animations except gentle fades.',
-			status: 'Mostly complete. Needs polishing.',
-			icon: 'reduce-motion'
-		},
-		{
 			title: 'Forced Colors',
 			desc: 'If you’re on windows, the site will respect Windows Forced Colors Mode, overriding all styling to the colors you select in your OS settings.',
-			status: 'Planned',
+			status: 'Mostly complete, needs testing',
 			icon: 'forced-colors'
-		},
-		{
-			title: 'Keyboard Navigation',
-			desc: 'The site is fully navigable using only a keyboard. In addition, all content is optimized for screen readers.',
-			status: 'Mostly complete. Needs additional testing.',
-			icon: 'keyboard-navigation'
-		},
-		{
-			title: 'Color Scheme',
-			desc: 'The site automatically adopts your system theme to minimize eye strain. in addition, colors are tuned across both themes to provide sufficient contrast for comfortable viewing.',
-			status: 'Mostly complete. Colors need a bit more tuning, especially on dark mode.',
-			icon: 'color-scheme'
 		},
 		{
 			title: 'Text Resize',
 			desc: 'Text is defined relative to your browser’s root font size, so depending on how you’ve configured your browser, text will reflow to match your desired reading size.',
-			status: 'Mostly complete. Font scales need a bit more tuning.',
+			status: 'Mostly complete, font scales need more tuning',
 			icon: 'text-resize'
 		},
 		{
 			title: 'Typography',
 			desc: 'Typography is balanced with additional line height and kerning to improve readability.',
-			status: 'Mostly complete, but needs more tuning in the fine details.',
+			status: 'Mostly complete, layouts need more tuning',
 			icon: 'typography'
+		},
+		{
+			title: 'Reduce Motion',
+			desc: 'The site respects OS-level Reduce Motion requests, eliminating all animations except gentle fades.',
+			status: 'Complete',
+			icon: 'reduce-motion'
+		},
+		{
+			title: 'Keyboard Navigation',
+			desc: 'The site is fully navigable using only a keyboard. In addition, all content is optimized for screen readers.',
+			status: 'Complete',
+			icon: 'keyboard-navigation'
+		},
+		{
+			title: 'Color Scheme',
+			desc: 'The site automatically adopts your system theme to minimize eye strain. in addition, colors are tuned across both themes to provide sufficient contrast for comfortable viewing.',
+			status: 'Complete',
+			icon: 'color-scheme'
 		},
 		{
 			title: 'Reader',
 			desc: 'Text-based pages (such as those in the Writings section) are optimized for Reader modes, allowing further user customization for a comfortable reading environment.',
-			status: 'Complete, but needs additional testing.',
+			status: 'Complete',
 			icon: 'reader'
+		},
+		{
+			title: 'Increase Contrast',
+			desc: 'If you’re on macOS, the site will respect OS-level Increase Contrast mode, heighting the distinction between colors and outlining click targets across all pages.',
+			status: 'In development, not yet available',
+			icon: 'increase-contrast'
 		}
 	];
 </script>
@@ -64,8 +64,8 @@
 	</p>
 	<p>
 		My goal is to have this site be 100% compliant with WCAG 2.2 AA by default, and reach AAA
-		compliance certain user configurations are applied. While I’m not there yet, this page documents
-		my progress on a number of accessibility features:
+		compliance when certain user configurations are applied. While I’m not there yet, this page
+		documents my progress on a number of accessibility features:
 	</p>
 
 	{#each features as feature}
@@ -116,6 +116,11 @@
 		small {
 			color: var(--green-11);
 		}
+	}
+
+	hr {
+		max-width: 75ch;
+		margin-inline: 0 auto;
 	}
 
 	@media (max-width: 850px) {
