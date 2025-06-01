5c93da9880962b5e1952faffd97296d5313dcee9
Kai Pepler <hello@kaipepler.com>
2025-06-01 15:07:49 -0400

    RSS update

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -8,27 +8,31 @@ export async function GET({ fetch }) {
 	const headers = { 'Content-Type': 'application/xml' };
 
 	const xml = `
-		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
-			<channel>
-				<title>${config.title}</title>
-				<description>${config.description}</description>
-				<link>${config.url}</link>
-				<atom:link href="${config.url}/rss.xml" rel="self" type="application/rss+xml"/>
+		<feed xmlns="http://www.w3.org/2005/Atom">
+      <title>${config.title}</title>
+      <subtitle>Articles on accessibility, design systems, and development</subtitle>
+      <link href="${config.url}/rss.xml" rel="self"/>
+      <link href="${config.url}"/>
+      <updated>${new Date().toISOString()}</updated>
+      <id>${config.url}</id>
+      <author>
+        <name>Kai Pepler</name>
+        <email>hello@kaipepler.com</email>
+      </author>
 				${articles
 					.map(
 						(article) => `
-						<item>
+						<entry>
 							<title>${article.title}</title>
 							<description>${article.description}</description>
-							<link>${config.url}/writing/${article.slug}</link>
-							<guid isPermaLink="true">${config.url}/writing/${article.slug}</guid>
-							<pubDate>${new Date(article.date).toUTCString()}</pubDate>
-						</item>
+							<link href="${config.url}/writing/${article.slug}"/>
+              <id>${config.url}/writing/${article.slug}</id>
+							<updated>${new Date(article.date).toUTCString()}</updated>
+						</entry>
 					`
 					)
 					.join('')}
-			</channel>
-		</rss>
+		</feed>
 	`.trim();
 
 	return new Response(xml, { headers });
